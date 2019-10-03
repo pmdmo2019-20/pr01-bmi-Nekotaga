e1eabcf4439ddb3f60008575b0704cd1b591317d
Unknown <eslizon@hotmail.com>
2019-10-03 22:33:27 +0200

    Body Mass Index
    No está acabado del todo porque me falla la fórmula por alguna razón que desconozco, aunque la lógica debería estar bien. Eso sí, no he podido probar los tests por culpa de ésto...

--- app/src/main/java/es/iessaladillo/pedrojoya/pr01/bmi/BmiCalculator.java
+++ app/src/main/java/es/iessaladillo/pedrojoya/pr01/bmi/BmiCalculator.java
@@ -11,16 +11,32 @@ public class BmiCalculator {
      * @return The body mass index (BMI)
      */
     public float calculateBmi(float weightInKgs, float heightInMeters) {
+        float bmi;
+        bmi = (float)(weightInKgs/(Math.pow(heightInMeters,2)));
+        return bmi;
         // TODO
     }
-
 
     /**
      * @param bmi Body mass index to get clasification from
      * @return A BmiClasification enum with the clasification of BMI
      */
     public BmiClasification getBmiClasification(float bmi) {
-        // TODO
+        BmiClasification calculatedBmi;
+        if (bmi < 18.5) {
+            calculatedBmi = BmiClasification.LOW_WEIGHT;
+        }else if (bmi >= 18.5 && bmi < 25) {
+            calculatedBmi = BmiClasification.NORMAL_WEIGHT;
+        }else if (bmi >= 25 && bmi < 30) {
+            calculatedBmi = BmiClasification.OVERWWEIGHT;
+        }else if (bmi >= 30 && bmi < 35) {
+            calculatedBmi = BmiClasification.OBESITY_GRADE_1;
+        }else if (bmi >= 35 && bmi < 40) {
+            calculatedBmi = BmiClasification.OBESITY_GRADE_2;
+        }else{
+            calculatedBmi = BmiClasification.OBESITY_GRADE_3;
+        }
+        return calculatedBmi;
     }
 
     public enum BmiClasification {
--- app/src/main/java/es/iessaladillo/pedrojoya/pr01/ui/main/MainActivity.java
+++ app/src/main/java/es/iessaladillo/pedrojoya/pr01/ui/main/MainActivity.java
@@ -1,9 +1,16 @@
 package es.iessaladillo.pedrojoya.pr01.ui.main;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+
 import es.iessaladillo.pedrojoya.pr01.R;
+import es.iessaladillo.pedrojoya.pr01.bmi.BmiCalculator;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,9 +18,69 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main_activity);
-        // TODO
+        clickBtnReset();
+        clickBtnCalculate();
+    }
+
+    private void clickBtnReset() {
+        Button btnReset = ActivityCompat.requireViewById(this, R.id.btnReset);
+        btnReset.setOnClickListener(r -> reset());
+    }
+    private void reset(){
+        EditText txtHeight = ActivityCompat.requireViewById(this,R.id.txtHeight);
+        EditText txtWeight = ActivityCompat.requireViewById(this,R.id.txtWeight);
+        ImageView image = ActivityCompat.requireViewById(this,R.id.image);
+        TextView txtViewMsg = ActivityCompat.requireViewById(this,R.id.txtViewMsg);
+        txtHeight.setText("");
+        txtWeight.setText("");
+        image.setImageResource(R.drawable.bmi);
+        txtViewMsg.setText("");
     }
 
     // TODO
+    private void clickBtnCalculate(){
+        Button btnCalculate = ActivityCompat.requireViewById(this, R.id.btnCalculate);
+        btnCalculate.setOnClickListener(c -> calculate());
+    }
+    private void calculate(){
+        EditText txtHeight = ActivityCompat.requireViewById(this,R.id.txtHeight);
+        EditText txtWeight = ActivityCompat.requireViewById(this,R.id.txtWeight);
+        TextView txtViewMsg = ActivityCompat.requireViewById(this,R.id.txtViewMsg);
+        ImageView image = ActivityCompat.requireViewById(this,R.id.image);
+        BmiCalculator.BmiClasification bmiEnum;
+        float result;
+
+        BmiCalculator bmi = new BmiCalculator();
+        result = bmi.calculateBmi(Float.parseFloat(txtHeight.getText().toString()), Float.parseFloat(txtWeight.getText().toString()));
+        bmiEnum = bmi.getBmiClasification(result);
+        switch (bmiEnum){
+            case LOW_WEIGHT:
+                txtViewMsg.setText(""+ result);
+                image.setImageResource(R.drawable.underweight);
+                break;
+            case NORMAL_WEIGHT:
+                txtViewMsg.setText("2");
+                image.setImageResource(R.drawable.normal_weight);
+                break;
+            case OVERWWEIGHT:
+                txtViewMsg.setText("3");
+                image.setImageResource(R.drawable.overweight);
+                break;
+            case OBESITY_GRADE_1:
+                txtViewMsg.setText("4");
+                image.setImageResource(R.drawable.obesity1);
+                break;
+            case OBESITY_GRADE_2:
+                txtViewMsg.setText("5");
+                image.setImageResource(R.drawable.obesity2);
+                break;
+            case OBESITY_GRADE_3:
+                txtViewMsg.setText("6");
+                image.setImageResource(R.drawable.obesity3);
+                break;
+        }
+
+
+    }
 
 }
